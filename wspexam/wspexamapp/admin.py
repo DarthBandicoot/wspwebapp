08a1c2d9452237fbba4e880bc382022b1c819d8f
Lewis Collins <lew.collins94@gmail.com>
2019-04-05 15:08:58 +0100

    added admin pages to check if users create on form submission

--- wspexam/wspexamapp/admin.py
+++ wspexam/wspexamapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pupil, Teacher
+
+
+@admin.register(Pupil)
+class PupilAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
